2d0cbd7c606c3d1dd9864cbe161cec50623b18f9
David Krause <david@code0.xyz>
2020-08-22 10:34:12 +0200

    fix for std async udp
    Signed-off-by: David Krause <david@code0.xyz>

--- multicast.nim
+++ multicast.nim
@@ -8,7 +8,7 @@
 #
 ## procs to work with multicast groups and ip broadcast
 ## tested on linux, windows and freebsd
-import net, os, nativesockets
+import net, os, nativesockets, asyncnet
 
 const IPPROTO_IP = 0
 const IPPROTO_IPV6 = 41
@@ -17,21 +17,21 @@ when defined windows:
   from winlean import In_Addr, inet_addr, setSockOpt# , # In6Addr
   # Old windows
   # const IP_ADD_MEMBERSHIP  = 5
-  # const IP_DROP_MEMBERSHIP = 6 
+  # const IP_DROP_MEMBERSHIP = 6
   # const IP_MULTICAST_TTL = 3
   # New windows
   # TODO (my) mingw crosscompiler misses In6Addr so defined here
   type
     In6Addr = object
       bytes: array[0..15, char]
-  const 
+  const
     IP_ADD_MEMBERSHIP  = 12
     IP_DROP_MEMBERSHIP = 13
     IP_MULTICAST_TTL = 10
     IPV6_JOIN_GROUP = 12 # TODO ipv6 needs testing
     IPV6_LEAVE_GROUP = 13 # TODO ipv6 needs testing
 else:
-  from posix import In_Addr, inet_addr, setSockOpt, In6Addr, 
+  from posix import In_Addr, inet_addr, setSockOpt, In6Addr,
     IPV6_JOIN_GROUP, IPV6_LEAVE_GROUP, inet_pton # , Tipv6_mreq
   when defined(FreeBSD) or defined(MacOS) or defined(MacOSX): # TODO check if MacOSX is needet here.
     const
@@ -50,13 +50,13 @@ else:
       IP_MULTICAST_TTL = 33
       # IPV6_JOIN_GROUP = 0 # TODO ipv6 needs testing
       # IPV6_LEAVE_GROUP = 0 # TODO ipv6 needs testing
-type 
+type
   ip_mreq = object {.pure, final.}
     imr_multiaddr*: InAddr
     imr_interface*: InAddr
   ipv6_mreq = object {.pure, final.} # there is in posix but missing in winlean!
     ipv6mr_multiaddr*: In6Addr ## IPv6 multicast address.
-    ipv6mr_interface*: cint ## Interface index.    
+    ipv6mr_interface*: cint ## Interface index.
 
 proc isMulticastAddress*(ipAddr: IpAddress): bool =
   ## returns true wether the given IpAddress is a ipv4 or ipv6 multicast
@@ -67,17 +67,17 @@ proc isMulticastAddress*(ipAddr: IpAddress): bool =
     return firstByte.shr(4) == 0x0E # 1110
   of IpAddressFamily.IPv6:
     let firstByte = ipAddr.address_v6[0]
-    return firstByte == 0xFF 
+    return firstByte == 0xFF
 
 proc isMulticastAddress*(group: string): bool =
   let ipAddr = parseIpAddress(group)
   return ipAddr.isMulticastAddress()
 
-proc joinGroup*(fd: SocketHandle, ipAddr: IpAddress, ttl = 255): bool = 
+proc joinGroup*(fd: SocketHandle, ipAddr: IpAddress, ttl = 255): bool =
   ## Instructs the os kernel to join a multicast group.
   ## returns true if sucessfull
   ## false otherwise
-  ## 
+  ##
   # Values for TTL:
   #  TTL     Scope
   # ----------------------------------------------------------------------
@@ -93,7 +93,7 @@ proc joinGroup*(fd: SocketHandle, ipAddr: IpAddress, ttl = 255): bool =
     mreq.imr_multiaddr.s_addr = inet_addr($ipAddr)
     mreq.imr_interface.s_addr= htonl(INADDR_ANY)
     var res = setSockOpt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, addr mreq, sizeof(ip_mreq).SockLen)
-    if res != 0: 
+    if res != 0:
       return false
     fd.setSockOptInt(IPPROTO_IP, IP_MULTICAST_TTL, ttl)
     return true
@@ -103,35 +103,39 @@ proc joinGroup*(fd: SocketHandle, ipAddr: IpAddress, ttl = 255): bool =
       mreq6.ipv6mr_multiaddr.bytes = cast[array[0..15, char]](ipAddr.address_v6)
     else:
       mreq6.ipv6mr_multiaddr.s6_addr = cast[array[0..15, char]](ipAddr.address_v6)
-    mreq6.ipv6mr_interface  = 0 # let os choose right interface; TODO?    
+    mreq6.ipv6mr_interface  = 0 # let os choose right interface; TODO?
     var res = setSockOpt(fd, IPPROTO_IPV6, IPV6_JOIN_GROUP, addr mreq6, sizeof(ipv6_mreq).SockLen)
-    return res == 0 
+    return res == 0
 
-proc joinGroup*(socket: Socket, ipAddr: IpAddress, ttl = 255): bool = 
+proc joinGroup*(socket: Socket, ipAddr: IpAddress, ttl = 255): bool =
   return joinGroup(socket.getFd(), ipAddr, ttl)
 
-proc joinGroup*(socket: Socket, group: string, ttl = 255): bool = 
+proc joinGroup*(socket: Socket, group: string, ttl = 255): bool =
   ## socket.joinGroup("239.2.3.4")
   let ipAddr = group.parseIpAddress()
   return socket.joinGroup(ipAddr, ttl)
+
+proc joinGroup*(socket: AsyncSocket, group: string, ttl = 255): bool =
+  let ipAddr = group.parseIpAddress()
+  return joinGroup(socket.getFd(), ipAddr, ttl)
 
 proc leaveGroup*(fd: SocketHandle, ipAddr: IpAddress): bool =
   ## Instructs the os kernel to leave a multicast group.
   ## returns true if sucessfull
   ## false otherwise
   case ipAddr.family
-  of IpAddressFamily.IPv4:  
+  of IpAddressFamily.IPv4:
     var mreq = ip_mreq()
     mreq.imr_multiaddr.s_addr = inet_addr($ipAddr)
     mreq.imr_interface.s_addr = htonl(INADDR_ANY)
     var res = setSockOpt(fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, addr mreq, sizeof(ip_mreq).SockLen)
-    return res == 0 
+    return res == 0
   of IpAddressFamily.IPv6:
     var mreq6 = ipv6_mreq()
     when defined windows:
       mreq6.ipv6mr_multiaddr.bytes = cast[array[0..15, char]](ipAddr.address_v6)
     else:
-      mreq6.ipv6mr_multiaddr.s6_addr = cast[array[0..15, char]](ipAddr.address_v6)    
+      mreq6.ipv6mr_multiaddr.s6_addr = cast[array[0..15, char]](ipAddr.address_v6)
     mreq6.ipv6mr_interface =  0 # let os choose right interface; TODO?
     var res = setSockOpt(fd, IPPROTO_IPV6, IPV6_LEAVE_GROUP, addr mreq6, sizeof(ipv6_mreq).SockLen)
     return res == 0
@@ -144,14 +148,18 @@ proc leaveGroup*(socket: Socket, group: string): bool =
   let ipAddr = group.parseIpAddress()
   return socket.leaveGroup(ipAddr)
 
+proc leaveGroup*(socket: AsyncSocket, group: string): bool =
+  let ipAddr = group.parseIpAddress()
+  return leaveGroup(socket.getFd(), ipAddr)
+
 proc enableBroadcast*(fd: SocketHandle, enable: bool) =
   ## enables the socket for broadcast
   ## some systems (linux, windows) allow sending to the global broadcast (255.255.255.255)
   ## while others (freebsd) only allow sending to the more specific broadcast eg 192.168.2.255
   let broadcastEnable = if enable: 1 else: 0
-  setsockoptint(fd, SOL_SOCKET.int, SO_BROADCAST.int,  broadcastEnable);  
+  setsockoptint(fd, SOL_SOCKET.int, SO_BROADCAST.int,  broadcastEnable);
 
-proc enableBroadcast*(socket: Socket, enable: bool) =
+proc enableBroadcast*(socket: Socket | AsyncSocket, enable: bool) =
   socket.getFd.enableBroadcast(enable)
 
 when isMainModule:
--- multicast.nimble
+++ multicast.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.4"
+version       = "0.1.5"
 author        = "enthus1ast"
 description   = "join and leave multicast groups / broadcast"
 license       = "MIT"
